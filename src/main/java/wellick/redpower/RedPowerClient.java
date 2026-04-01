5edc5bb7ba95496db39beee5f86f8a4f5ed341be
wellick <aurel0000@proton.me>
2026-04-01 17:41:31 +0200

    initial commit

---
+++ src/main/java/wellick/redpower/RedPowerClient.java
@@ -0,0 +1,10 @@
+package wellick.redpower;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RedPowerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/wellick/redpower/RedpowerDataGenerator.java
@@ -0,0 +1,11 @@
+package wellick.redpower;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RedpowerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
